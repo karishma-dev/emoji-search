dd7781113d98ab528970c80d5185304691ebef53
Krrish105 <jainkarishma76@gmail.com>
2022-04-08 18:10:04 +0530

    Added the components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import logo from './logo.svg';
+import Search from './components/Search';
+import DisplayEmojis from './components/DisplayEmojis';
+import filterEmoji from './filterEmoji';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+
+  // State for input text
+  const [text, setText] = useState('');
+
+  // Function to handle text change in input bar
+  const handleChange = (e) => {
+      setText(e.target.value)
+  }
+
   return (
     <div className="App">
+      <h1>Emoji Search</h1>
+      <Search handleChange = {handleChange}/>
+      <DisplayEmojis emojiList={filterEmoji(text)} />
     </div>
   );
 }
